11fe9a55536f48800ce15293b696a0c8d8b65446
Mag Core <achillejcristof@gmail.com>
2026-09-04 00:16:18 +0200

    Delete core directorys

--- core/interpolate.ts
+++
@@ -1,6 +0,0 @@
-export const lerp = (a: number, b: number, t: number) => a + (b - a) * t
-export const damp = (current: number, target: number, lambda = 0.08, dt = 1) =>
-  lerp(current, target, 1 - Math.exp(-lambda * dt))
-export const edgeInterpolate = (from: number, to: number, t: number) =>
-  from + (to - from) * (1 - Math.pow(2, -10 * t))
-export const smoothStep = (t: number) => t * t * (3 - 2 * t)
