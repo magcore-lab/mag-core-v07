f246cd76cc95d526e2dee0af3a64ee8055ce5e89
Mag Core <achillejcristof@gmail.com>
2026-09-04 08:06:54 +0200

    Add multiverse page with basic layout

---
+++ app/multiverse/page.tsx
@@ -0,0 +1,8 @@
+export default function Page(){
+  return (
+    <div style={{background:"black",color:"white",minHeight:"100vh",display:"grid",placeItems:"center"}}>
+      <h1>MULTIVERSE OPEN</h1>
+      <a href="/" style={{marginTop:20,color:"#666"}}>← BACK</a>
+    </div>
+  )
+}
