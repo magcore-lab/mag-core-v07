d197205e1a6f2adcc0738077be80dd33382a6ccc
Mag Core <achillejcristof@gmail.com>
2026-09-07 22:01:06 +0200

    Refactor dynamic import of Desktop component

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,4 @@
-import dynamic from "next/dynamic";
-
-const Desktop = dynamic(() => import("./components/Desktop"), {
-  ssr: false,
-  loading: () => (
-    <div className="flex h-screen w-screen items-center justify-center bg-black">
-      <div className="font-mono text-[10px] tracking-[0.3em] text-white/20">
-        MAG CORE V07 // INITIALIZING MULTIVERSE_OS...
-      </div>
-    </div>
-  ),
-});
-
+import Desktop from "./components/Desktop";
 export default function Page() {
   return <Desktop />;
 }
