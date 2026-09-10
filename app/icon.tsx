982e8ded9c193a0579587e9c7b80efddcd0563ea
Mag Core <achillejcristof@gmail.com>
2026-09-10 07:50:05 +0200

    Modify Icon component to render ImageResponse
    Updated Icon component to return an ImageResponse with a specified image.

--- app/icon.tsx
+++ app/icon.tsx
@@ -1,5 +1,12 @@
+
+import { ImageResponse } from "next/og"
+export const runtime = "edge"
 export const size = { width: 512, height: 512 }
 export const contentType = "image/png"
-export default function Icon() { return null }
-// Utilise ton webp optimisé déjà uploadé dans /public
-// Next va servir /core-off-512.webp comme favicon autos
+export default function Icon(){
+  return new ImageResponse(
+    <div style={{background:"black",width:"100%",height:"100%",display:"flex",alignItems:"center",justifyContent:"center"}}>
+      <img src="https://mag-core-v07.vercel.app/core-off-512.webp" width="512" height="512" alt="" />
+    </div>, { ...{width:512,height:512} }
+  )
+}
