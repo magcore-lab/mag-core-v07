fa7d8ca00e6200509fd6c845c687bbe3f0ea3ec4
Mag Core <achillejcristof@gmail.com>
2026-09-10 07:48:15 +0200

    Add icon component with size and content type

---
+++ app/icon.tsx
@@ -0,0 +1,5 @@
+export const size = { width: 512, height: 512 }
+export const contentType = "image/png"
+export default function Icon() { return null }
+// Utilise ton webp optimisé déjà uploadé dans /public
+// Next va servir /core-off-512.webp comme favicon autos
